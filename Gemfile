612c8d30555b2db6444c611fa8f1f036b04a1c07
Swati <swatisaoji1@gmail.com>
2015-10-27 18:12:55 -0700

    added facebook authentication

--- Gemfile
+++ Gemfile
@@ -13,9 +13,14 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+#=====================================
 # login module
 gem 'devise'
 
+#omniauth allow third party login
+gem 'omniauth-facebook'
+
+#=====================================
 # javascript runtime
 gem 'therubyracer'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         
+  def self.from_omniauth(auth)
+    where(auth.slice(provider: auth.provider, uid: auth.uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+    end
+  end
 end
