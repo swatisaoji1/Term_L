84e3649c7092619a42f03d94a96679a9a8a232a1
Swati <swatisaoji1@gmail.com>
2015-10-30 11:36:46 -0700

    checking omniauth without storing username

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < ActiveRecord::Base
     where(auth.slice(provider: auth.provider, uid: auth.uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.username = auth.info.nickname
+      # user.username = auth.info.nickname
     end
   end
+  
+  
 end
