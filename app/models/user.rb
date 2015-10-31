364d04527a4bd773f93db2aee1cc5875cc1f7634
Swati <swatisaoji1@gmail.com>
2015-10-31 15:15:53 -0700

    Fixed the persistence of facebook finall (important working landmark)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,31 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
+  devise :database_authenticatable, :registerable, :omniauthable,  :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
          
          
   def self.from_omniauth(auth)
     logger.debug "*****************in from_omniauth************"
     logger.debug auth["info"]["email"]
-    where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
+    # find the user by email id if exists
+    user = User.find_by(email: auth["info"]["email"])
+    logger.debug user.inspect
+    
+    if user and user.confirmed?
+      logger.debug "confirmed user"
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.email = auth["info"]["email"]
+      return user
+    end
+    
+    where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |newuser|\
+      newuser.skip_confirmation!
+      newuser.provider = auth["provider"]
+      newuser.uid = auth["uid"]
+      newuser.email = auth["info"]["email"]
+      newuser.password = Devise.friendly_token[0,20]
+      newuser.name = auth ["info"]["name"]
     end
   end
   
