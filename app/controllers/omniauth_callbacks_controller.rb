b1f6b90d82fe28bac7cb99f57edb1e82c17055c2
Swati <swatisaoji1@gmail.com>
2015-10-30 18:20:31 -0700

    added sign up validation and fixed minor routing issues

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,18 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
+    logger.debug "I am in method ********facebook****************"
+    logger.debug User.from_omniauth(request.env["omniauth.auth"]).inspect
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
+      logger.debug user.email
       sign_in_and_redirect user, notice: "Signed in"
     else
+      session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
     end
-    
   end
   
+  
+
   alias_method :facebook, :all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,18 @@ class User < ActiveRecord::Base
     where(auth.slice(provider: auth.provider, uid: auth.uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.username = auth.info.nickname
+      user.email = auth.info.email
     end
   end
   
-  
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end 
+  end
 end
