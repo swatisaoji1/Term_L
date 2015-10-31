e30ce6cdf65a16f8d2eac0ca8c570b03e151b7c3
Swati <swatisaoji1@gmail.com>
2015-10-30 19:30:02 -0700

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,11 @@ class User < ActiveRecord::Base
          
   def self.from_omniauth(auth)
     logger.debug "*****************in from_omniauth************"
-    logger.debug auth.provider
-    logger.debug auth.uid
-    logger.debug auth.info.inspect
-    logger.debug auth.info.email
+    logger.debug auth["info"]["email"]
     where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.email = auth["info"]["email"]
-      user.save
     end
   end
   
