c779b21b33901b13361f3f9a14195e0078271ca3
amruta deshmukh <amruta1209@gmail.com>
2015-11-04 17:21:29 -0800

    ading the book details

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :dashboard
   root 'welcome#index'
   get 'books' => 'books#index'
+  get 'books/:id' =>'books#show', as: :book
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
