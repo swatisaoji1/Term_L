12c5aac2fa95f45e0c2960798a6b44df394aec6b
Swati <swatisaoji1@gmail.com>
2015-11-21 17:26:33 -0800

    added new book form and method to add the book to db in controller. chnaged route that may break books/show (please check)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,20 @@ class BooksController < ApplicationController
     @authors = @book.authors
   end
   
+  def new
+   @book = Book.new
+  end
+ 
+  def create
+   @book = Book.new(book_params)
+   if @book.save
+     redirect_to books_path, :notice => "Your Book Was Posted"
+   else
+     render "new"
+   end
+  end
+   private
+ def book_params
+   params.require(:book).permit(:title, :description, :isbn, :image_path, :price)
+ end
 end
