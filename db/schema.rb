417f3d1b3ca73c329f6debfeb6c071d3390373f0
Swati <swatisaoji1@gmail.com>
2015-11-22 18:00:35 -0800

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151122171512) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,9 +47,9 @@ ActiveRecord::Schema.define(version: 20151122171512) do
     t.string   "image_path"
     t.string   "edition"
     t.float    "price",                    null: false
+    t.integer  "quantity",                 null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.integer  "quantity"
     t.string   "sale_type"
     t.integer  "user_id"
   end
