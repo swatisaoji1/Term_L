ce7e3f1cb84adf8daed4254bbbbfb57866f624e0
amruta deshmukh <amruta1209@gmail.com>
2015-11-26 14:07:40 -0800

    made some changes to the books controller and the books#new html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,12 +16,24 @@ class BooksController < ApplicationController
   end
  
   def create
-   @book = Book.new(book_params)
-   @id = current_user.id
-   logger.debug "The user id : "
-   logger.debug @id
 
-   puts @id
+    puts "#{book_params["tags"]}"
+
+    tags_string = book_params["tags"]
+    tags_arr = tags_string.split(",")
+    puts "#{tags_arr}"
+    #book_params["tags"] = tags_arr
+    tags_arr.each do |x| 
+      puts "x: #{x}"
+              end
+
+    puts "#{book_params["tags"]}"
+
+    puts"#{book_params}"
+
+
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
    if @book.save
      redirect_to books_path, :notice => "Your Book Was Posted"
    else
@@ -30,6 +42,6 @@ class BooksController < ApplicationController
   end
    private
  def book_params
-   params.require(:book).permit(:title, :description, :isbn, :image_path, :price, :quantity)
+   params.require(:book).permit(:title, :description, :isbn, :image_path, :price, :tags, :quantity, :sale_type)
  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
 class Book < ActiveRecord::Base
-	has_and_belongs_to_many :categories
-	has_and_belongs_to_many :publishers
-	has_and_belongs_to_many :authors
-	belongs_to :users
-	has_many :postings
-	has_many :order_entries
+  
+  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :publishers
+  has_and_belongs_to_many :authors
+  belongs_to :users
+  has_many :postings
+  has_many :order_entries
+
 end
