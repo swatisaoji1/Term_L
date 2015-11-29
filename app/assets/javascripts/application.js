f8fc08f78544a5472c7e31fc8b88d79d458231a6
Swati <swatisaoji1@gmail.com>
2015-11-29 08:17:02 -0800

    added basic search , modified pusher

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,21 +19,20 @@
 
 
 $(document).ready(function() {
-	toastr.options = {
-	  "closeButton": false,
-	  "debug": false,
-	  "newestOnTop": true,
-	  "progressBar": false,
-	  "positionClass": "toast-top-right",
-	  "preventDuplicates": false,
-	  "onclick": null,
-	  "showDuration": "300",
-	  "hideDuration": "1000",
-	  "timeOut": "5000",
-	  "extendedTimeOut": "1000",
-	  "showEasing": "swing",
-	  "hideEasing": "linear",
-	  "showMethod": "fadeIn",
-	  "hideMethod": "fadeOut"
-}
+			toastr.options = {
+			  "closeButton": true,
+			  "debug": true,
+			  "newestOnTop": true,
+			  "progressBar": true,
+			  "positionClass": "toast-bottom-right",
+			  "preventDuplicates": true,
+			  "showDuration": "300",
+			  "hideDuration": "1000",
+			  "timeOut": "5000",
+			  "extendedTimeOut": "1000",
+			  "showEasing": "swing",
+			  "hideEasing": "linear",
+			  "showMethod": "fadeIn",
+			  "hideMethod": "fadeOut"
+			}
 	});
